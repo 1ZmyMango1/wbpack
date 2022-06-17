7132bb33adb104ab300fbed0840150d189329c9b
zhangmengyang <2655419131@qq.com>
2022-06-17 20:23:43 +0800

    完成了处理图片功能

--- webpack练习/src/main.js
+++ webpack练习/src/main.js
@@ -8,3 +8,18 @@ tab();
 import "./styles/index.css";
 
 import "./styles/index.less";
+
+import gifStr from "./assets/1.gif";
+import pngStr from "./assets/logo_small.png";
+
+const gif = document.createElement("img");
+const png = document.createElement("img");
+
+gif.src = gifStr;
+png.src = pngStr;
+
+document.body.appendChild(gif);
+document.body.appendChild(png);
+
+// 引入字体图标文件
+import "./assets/fonts/iconfont.css";
--- webpack练习/webpack.config.js
+++ webpack练习/webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.less$/i,
         use: ["style-loader", "css-loader", "less-loader"],
       },
+      {
+        test: /\.(png|jpg|gif|jpeg)$/i,
+        type: "asset",
+      },
     ],
   },
 };
